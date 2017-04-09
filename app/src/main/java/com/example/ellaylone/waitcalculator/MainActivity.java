5e208d4435b4aecb6c03d010a99e1e99ee19c9fc
Ellaylone <ellaylone@yandex-team.ru>
2017-04-09 15:57:20 +0300

    main timer

--- app/src/main/java/com/example/ellaylone/waitcalculator/MainActivity.java
+++ app/src/main/java/com/example/ellaylone/waitcalculator/MainActivity.java
@@ -12,23 +12,45 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
+    final int DATABASE_VERSION = 3;
     final String EMPTY_VIEW = "";
     String startDate = EMPTY_VIEW;
     String endDate = EMPTY_VIEW;
+    String todayDate = EMPTY_VIEW;
+    int difference = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupDb();
         setupDatePickers();
+
+        setupDb();
+
+        displayMainTimer(difference);
+    }
+
+
+    private String getCurrentDate() {
+        final Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+
+        return "" + day + "." + (month + 1) + "." + day + "T" + hour + ":" + minute + ":" + second;
     }
 
     private void setupDb() {
-        DbHelper dbHelper = new DbHelper(MainActivity.this, "WaitCalculator", null, 2);
+        DbHelper dbHelper = new DbHelper(MainActivity.this, "WaitCalculator", null, DATABASE_VERSION);
         SQLiteDatabase db;
         try {
             db = dbHelper.getWritableDatabase();
@@ -55,14 +77,15 @@ public class MainActivity extends AppCompatActivity {
                 }
                 c.moveToNext();
             }
+            difference = calculateDifference(getCurrentDate(), endDate);
             c.close();
         } else {
             ContentValues newValues = new ContentValues();
-            newValues.put("DATE_VALUE", "02.04.2017");
+            newValues.put("DATE_VALUE", "02.04.2017T13:00:00");
             newValues.put("DATE_TYPE", 1);
             db.insert("DATES", null, newValues);
             newValues.clear();
-            newValues.put("DATE_VALUE", "14.05.2017");
+            newValues.put("DATE_VALUE", "14.05.2017T15:55:00");
             newValues.put("DATE_TYPE", 2);
             db.insert("DATES", null, newValues);
         }
@@ -79,22 +102,109 @@ public class MainActivity extends AppCompatActivity {
         setupEndDatePicker();
     }
 
-    private String calculateDifference() {
-        String difference = "36";
-        //TODO calculate difference
-        return difference;
+    private int calculateDifference(String startDate, String endDate) {
+
+        int difference = 0;
+
+        String[] startParts = startDate.split("T");
+        String[] endParts = endDate.split("T");
+
+        if (startParts.length != 2 && endParts.length != 2) {
+            return difference;
+        }
+
+        String[] startDateParts = startParts[0].split("\\.");
+        String[] endDateParts = endParts[0].split("\\.");
+
+        String[] startTimeParts = startParts[1].split(":");
+        String[] endTimeParts = endParts[1].split(":");
+
+        if ((startDateParts.length != 3 && endDateParts.length != 3) || (startTimeParts.length != 3 && endDateParts.length != 3)) {
+            return difference;
+        }
+
+        int startYear = Integer.parseInt(startDateParts[2]);
+        int endYear = Integer.parseInt(endDateParts[2]);
+
+        int startMonth = Integer.parseInt(startDateParts[1]);
+        int endMonth = Integer.parseInt(endDateParts[1]);
+
+        int startDayOfMonth = Integer.parseInt(startDateParts[0]);
+        int endDayOfMonth = Integer.parseInt(endDateParts[0]);
+
+        int[] differenceParts = new int[3];
+        differenceParts[0] = endYear - startYear;
+        differenceParts[1] = endMonth - startMonth;
+        differenceParts[2] = endDayOfMonth - startDayOfMonth;
+
+        if (differenceParts[0] < 0 || (differenceParts[0] == 0 && differenceParts[1] < 0) || (differenceParts[0] == 0 && differenceParts[1] == 0 && differenceParts[2] < 0)) {
+            return difference;
+        }
+
+        int[] daysInMonth = new int[13];
+        daysInMonth[1] = 31;
+        daysInMonth[2] = 28;
+        daysInMonth[3] = 31;
+        daysInMonth[4] = 30;
+        daysInMonth[5] = 31;
+        daysInMonth[6] = 30;
+        daysInMonth[7] = 31;
+        daysInMonth[8] = 31;
+        daysInMonth[9] = 30;
+        daysInMonth[10] = 31;
+        daysInMonth[11] = 30;
+        daysInMonth[12] = 31;
+
+        int totalDayDifference = 0;
+        for (int i = 0; i <= differenceParts[1]; i++) {
+            int month = startMonth + i;
+            if (month > 12) {
+                month = 1;
+            }
+            totalDayDifference += daysInMonth[month];
+        }
+        totalDayDifference -= startDayOfMonth;
+        totalDayDifference -= daysInMonth[endMonth] - endDayOfMonth;
+
+        int startHour = Integer.parseInt(startTimeParts[0]);
+        int endHour = Integer.parseInt(endTimeParts[0]);
+
+        int startMinutes = Integer.parseInt(startTimeParts[1]);
+        int endMinutes = Integer.parseInt(endTimeParts[1]);
+
+        int startSeconds = Integer.parseInt(startTimeParts[2]);
+        int endSeconds = Integer.parseInt(endTimeParts[2]);
+
+        int totalSecondsDifference = endSeconds - startSeconds;
+        totalSecondsDifference += endMinutes * 60 - startMinutes * 60;
+        totalSecondsDifference += endHour * 60 * 60 - startHour * 60 * 60;
+
+        totalSecondsDifference = totalDayDifference * 24 * 60 * 60 + totalSecondsDifference;
+
+        return totalSecondsDifference;
     }
 
-    private void displayMainTimer(String difference) {
+    private void displayMainTimer(int difference) {
         TextView mainTimer = (TextView) findViewById(R.id.main_timer_text_view);
-        mainTimer.setText(difference);
+        if (difference > 0) {
+            int fullDays = difference / (60 * 60 * 24);
+            difference -= fullDays * (60 * 60 * 24);
+            int fullHours = difference / (60 * 60);
+            difference -= fullHours * (60 * 60);
+            int fullMinutes = difference / (60);
+            int fullSeconds = difference - fullMinutes * 60;
+            String mainTimerText = "" + fullDays + "d " + String.format("%02d", fullHours) + ":" + String.format("%3d", fullMinutes) + ":" + String.format("%3d", fullSeconds);
+            mainTimer.setText(mainTimerText);
+        } else {
+            mainTimer.setText("");
+        }
     }
 
     private void checkDates() {
-        if (startDate != EMPTY_VIEW && endDate != EMPTY_VIEW) {
-            displayMainTimer(calculateDifference());
+        if (endDate != EMPTY_VIEW) {
+//            displayMainTimer("" + calculateDifference(startDate, endDate));
         } else {
-            displayMainTimer(EMPTY_VIEW);
+//            displayMainTimer(EMPTY_VIEW);
         }
     }
 
